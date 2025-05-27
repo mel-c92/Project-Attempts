d1ffb214647cf54d3b8c4a86e7cb4cf423f123d7
Mel Cugnet <melanie.daras94@gmail.com>
2025-05-27 10:17:54 +0200

    mousemove done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mouseMove = document.querySelectorAll(".mouse");
+const liHover = document.querySelector("li");
 
-window.addEventListener(() => {
-  mouseMove.forEach(){
-    
-  };
+window.addEventListener("mousemove", (e) => {
+  mouseMove.forEach((mouse) => {
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+  });
 });
