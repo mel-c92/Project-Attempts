137bb724e15138bd588e316f2fe8e6adaf68c260
Mel Cugnet <melanie.daras94@gmail.com>
2025-07-09 11:56:49 +0200

    TO DO LIST opérationnelle, reste l'incrémentation de localStorage

--- exo_FS_alone/todolist/script.js
+++ exo_FS_alone/todolist/script.js
@@ -6,10 +6,17 @@ let addBtn = document.getElementById("add");
 let listLi = document.querySelector(".list > ul");
 
 // MON EVENEMENT
+// il ajoute à la liste chaque entrée de l'utilisateur
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let todo = enterTodo.value;
-  console.log(todo); //récupérer la valeur de l'input
 
-  
+  let newLi = document.createElement("li");
+  newLi.innerText += `${todo}`;
+  listLi.appendChild(newLi);
+
+  enterTodo.value = "";
 });
+
+// STOCKAGE DES DONNEES DANS LE NAVIGATEUR
+// usage de localstorage
